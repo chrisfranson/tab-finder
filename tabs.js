c11cb55664a59d9cfb13ae1ffabc25eb5f690ae1
Chris Franson <chris.franson@gmail.com>
2017-10-31 14:23:45 -0500

    List ALL the tabs! ✊
    List all tabs across all windows, even if there are 400 of them.

--- tabs.js
+++ tabs.js
@@ -3,16 +3,16 @@
  * listTabs to switch to
  */
 function listTabs() {
-  getCurrentWindowTabs().then((tabs) => {
+  getAllTabs().then((tabs) => {
     let tabsList = document.getElementById('tabs-list');
     let currentTabs = document.createDocumentFragment();
-    let limit = 5;
+    let limit = 400;
     let counter = 0;
 
     tabsList.textContent = '';
 
     for (let tab of tabs) {
-      if (!tab.active && counter <= limit) {
+      if (counter <= limit) {
         let tabLink = document.createElement('a');
 
         tabLink.textContent = tab.title || tab.id;
@@ -34,9 +34,13 @@ function getCurrentWindowTabs() {
   return browser.tabs.query({currentWindow: true});
 }
 
+function getAllTabs() {
+  return browser.tabs.query({});
+}
+
 document.addEventListener("click", (e) => {
   function callOnActiveTab(callback) {
-    getCurrentWindowTabs().then((tabs) => {
+    getAllTabs().then((tabs) => {
       for (var tab of tabs) {
         if (tab.active) {
           callback(tab, tabs);
@@ -58,9 +62,7 @@ document.addEventListener("click", (e) => {
 
     var tabId = +e.target.getAttribute('href');
 
-    browser.tabs.query({
-      currentWindow: true
-    }).then((tabs) => {
+    getAllTabs().then((tabs) => {
       for (var tab of tabs) {
         if (tab.id === tabId) {
           browser.tabs.update(tabId, {
